c7f406315cef58c9cff21d8911be64474d630bc7
Ankit Sah <sah.5ankit@gmail.com>
2025-07-23 15:22:32 +0545

    Implemented authentication model Auth0

---
+++ src/app/utils/authOptions.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        Auth0Provider({
+            clientId: process.env.AUTH0_CLIENT_ID as string,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+            issuer: process.env.AUTH0_ISSUER as string
+        })
+    ],
+    secret: process.env.NEXT_AUTH_SECRET
+}
